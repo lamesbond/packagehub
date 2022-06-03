0f81dd4ba1298826280458e2249254dbaa32c4d5
lamesbond <1418190515@qq.com>
2022-06-03 13:58:25 +0800

    完成项目创建

---
+++ src/main/java/com/liubusi/packagehub/PackagehubApplication.java
@@ -0,0 +1,13 @@
+package com.liubusi.packagehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackagehubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liubusi/packagehub/PackagehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liubusi.packagehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
